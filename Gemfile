6eb48b4c213beec69896a9fdb394ac953ee53145
ismail424 <cicas03@hotmail.com>
2022-02-14 19:52:59 +0100

    Better style

---
+++ Gemfile
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+# gem "rails"
+
+gem "sinatra", "~> 2.1"
+gem "sinatra-contrib", "~> 2.1"
+gem "puma", "~> 5.6"
+gem "sassc", "~> 2.4"
+gem "bcrypt", "~> 3.1"
+gem "sqlite3", "~> 1.4"
--- app.rb
+++ app.rb
@@ -1,14 +1,21 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'slim'
-require 'sqlite3'
-require 'bcrypt'
+require "sqlite3"
+require 'sassc'
+require_relative './utils'
 
 set :port, 5000
 set :bind, '0.0.0.0'
 set :sessions, enable
+set :session_secret, 'super secret'
 
+get('/css/style.css') do
+    scss :'scss/style', :style => :compressed
+end
 
 get("/") do
-    return slim(:index)
+    database = connect_to_db()
+    videogames = database.execute("SELECT * FROM VideoGames LIMIT 10;")
+    return slim(:index, locals: { videogames: videogames })
 end
