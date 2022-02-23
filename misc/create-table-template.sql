5b9cf0eb3fecfd6e1947873988921e0c4a22c66e
ismail424 <cicas03@hotmail.com>
2022-02-23 10:29:56 +0100

    Fixing database

--- misc/create-table-template.sql
+++ misc/create-table-template.sql
@@ -39,18 +39,48 @@ CREATE TABLE `Streamers`(
     `description` TEXT NOT NULL,
     `platform` TEXT NOT NULL,
     `favorite_game` INT NOT NULL,
-     FOREIGN KEY (`favorite_game`) REFERENCES `VideoGames`(`id`)
 );
 
 CREATE TABLE `VideoGames`(
     `id` integer primary key autoincrement,
-    `img_src` TEXT NOT NULL,
     `name` TEXT NOT NULL,
     `price` DOUBLE(8, 2) NOT NULL,
     `description` TEXT NOT NULL,
-    `realse_date` DATE NULL,
-    `genre` TEXT NOT NULL,
-    `platform` TEXT NOT NULL
-    
+    `realse_date` DATE NULL
 );
+
+CREATE TABLE `Genres`(
+    `id` integer primary key autoincrement,
+    `name` TEXT NOT NULL
+);
+
+CREATE TABLE `Platforms`(
+    `id` integer primary key autoincrement,
+    `name` TEXT NOT NULL
+);
+
+CREATE TABLE `Images`(
+    `id` integer primary key autoincrement,
+    `src` TEXT NOT NULL
+);
+
+CREATE TABLE `GenreToGame`(
+    `genre_id` INT NOT NULL,
+    `game_id` INT NOT NULL,
+    FOREIGN KEY(`game_id`) REFERENCES `VideoGames`(`id`),
+    FOREIGN KEY(`genre_id`) REFERENCES `Genres`(`id`)
+);
+CREATE TABLE `ImageToGame`(
+    `image_id` INT NOT NULL,
+    `game_id` INT NOT NULL,
+    FOREIGN KEY(`game_id`) REFERENCES `VideoGames`(`id`),
+    FOREIGN KEY(`image_id`) REFERENCES `Images`(`id`)
+);
+CREATE TABLE `PlatformToGame`(
+    `platform_id` INT NOT NULL,
+    `game_id` INT NOT NULL,
+    FOREIGN KEY(`game_id`) REFERENCES `VideoGames`(`id`),
+    FOREIGN KEY(`platform_id`) REFERENCES `Platforms`(`id`)
+);
+
 
